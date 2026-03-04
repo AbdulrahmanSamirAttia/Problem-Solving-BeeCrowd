672b66319aedb339a2d5dfbca2a1da0f0f8eaab0
AbdulRahmanSamir <abdelrahman.samir@waffarha.com>
2026-03-05 01:24:07 +0200

    1001.extremely_basic

---
+++ 1001.extremely_basic.py
@@ -0,0 +1,28 @@
+"""
+Problem
+    Read 2 variables, named A and B and make the sum of these two variables,
+    assigning its result to the variable X. Print X as shown below.
+    Print end line after the result otherwise you will get “Presentation Error”.
+
+Input
+    The input file will contain 2 integer numbers.
+
+Output
+    Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X,
+    according to the following example.
+
+Obs.:
+    don't forget the end line after all.
+"""
+
+
+""" Solution (A) """
+A = int(input())
+B = int(input())
+
+X = A + B
+
+print("X =", X )
+
+""" Solution (B) """
+print("X =", int(input()) + int(input()))
