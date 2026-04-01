31761ba096cce3441489bac7901377cf68737180
AbdulRahmanSamir <abdelrahman.samir@waffarha.com>
2026-04-01 12:41:20 +0200

    1014.consumption.py

---
+++ 1014.consumption.py
@@ -0,0 +1,22 @@
+"""
+Calculate a car's average consumption being provided
+the total distance traveled (in Km) and the spent fuel total (in liters).
+
+Input
+The input file contains two values:
+one integer value X representing the total distance (in Km) and
+the second one is a floating point number Y  representing the spent fuel total,
+with a digit after the decimal point.
+
+Output
+Present a value that represents the average consumption of a car with 3 digits
+after the decimal point, followed by the message "km/l".
+"""
+
+""" Solution (A) """
+total_distance = int(input().strip())
+spent_fuel = float(input().strip())
+
+average_consumption = total_distance / spent_fuel
+
+print(f"{average_consumption:.3f} km/l")
